bb7815cfc6660ab372b1ee44de5617f3673db97c
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-12-27 01:35:08 -0600

    feat: add basic layout for new game page

--- src/routes/(menu)/new/+page.svelte
+++ src/routes/(menu)/new/+page.svelte
@@ -1,1 +1,27 @@
-<section>New Game Page</section>
+<script>
+  import { goto } from "$app/navigation";
+
+  function goBack() {
+    if (window.history.length > 1) {
+      window.history.back();
+    } else {
+      goto("/");
+    }
+  }
+</script>
+
+<div class="flex h-full flex-col gap-y-2">
+  <header class="bg-[#acc] px-4 py-2 text-2xl font-semibold uppercase">
+    Decks
+  </header>
+  <h2 class="text-lg">
+    Don't fret, these settings can be adjusted later&hellip;
+  </h2>
+  <section class="flex-grow">
+    <div>Lorem ipsum</div>
+  </section>
+  <nav class="flex justify-between font-semibold uppercase">
+    <button on:click={goBack} class="nav-button">&larr;&nbsp;Back</button>
+    <a href="/game" class="nav-button">Next&nbsp;&rarr;</a>
+  </nav>
+</div>
