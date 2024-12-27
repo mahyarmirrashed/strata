65caaf72e03511b011638e861f875b259ec8e22b
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-12-27 01:16:06 -0600

    feat: update landing page

--- src/routes/(menu)/+page.svelte
+++ src/routes/(menu)/+page.svelte
@@ -1,17 +1,7 @@
 <nav
-  class="flex h-full flex-col justify-end gap-y-2 pb-8 font-semibold uppercase text-primary"
+  class="flex h-full flex-col justify-end gap-y-2 pb-6 font-semibold uppercase text-primary"
 >
-  <h1 class="mb-2 w-1/2 text-4xl">Reflect.<br />Reveal.<br />Relate.</h1>
-  <a href="/new" class="group mr-auto text-left text-lg"
-    >New Game<span
-      class="opacity-0 transition-opacity duration-200 group-hover:opacity-100"
-      >&nbsp;&rarr;</span
-    >
-  </a>
-  <a href="/load" class="group mr-auto text-left text-lg"
-    >Load Game<span
-      class="opacity-0 transition-opacity duration-200 group-hover:opacity-100"
-      >&nbsp;&rarr;</span
-    ></a
-  >
+  <h1 class="mb-2 w-1/2 px-2 text-4xl">Reflect.<br />Reveal.<br />Relate.</h1>
+  <a href="/new" class="nav-button mr-auto">New Game</a>
+  <a href="/load" class="nav-button mr-auto">Load Game</a>
 </nav>
